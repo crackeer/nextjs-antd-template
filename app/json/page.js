af9051e379f4875d6e37cc42de53aedf114dc002
simple <seekh@qq.com>
2024-05-01 09:38:51 +0800

    update

--- app/json/page.js
+++ app/json/page.js
@@ -5,6 +5,32 @@ import JSONEditor from '@/component/JSONEditor';
 import CopyToClipboard from '@/component/CopyToClipboard';
 import jsonToGo from "@/util/json-to-go";
 import * as XLSX from 'xlsx';
+
+const intoObjectList = (list) => {
+    let retData = []
+    for (var i in list) {
+        if (i > 0) {
+            let tmp = {}
+            for (var j in list[0]) {
+                tmp[list[0][j]] = list[i][j]
+            }
+            retData.push(tmp)
+        }
+    }
+    return retData
+}
+const getLocalJSONValue = () => {
+    try {
+        let value = localStorage.getItem('json-local-value') || ''
+        return JSON.parse(value)
+    } catch (e) {
+        return {}
+    }
+}
+const setLocalJSONValue = (value) => {
+    let raws = JSON.stringify(value)
+    localStorage.setItem('json-local-value', raws)
+}
 class Convert extends React.Component {
     jsonObject = null
     constructor(props) {
@@ -16,26 +42,14 @@ class Convert extends React.Component {
             convert: '',
         }
     }
-    componentDidMount = async () => {
-        await this.setState({
-            json: this.getLocalJSONValue()
-        })
+    initJSONEditor = (e) => {
+        this.jsonObject = e
         setTimeout(() => {
-            this.jsonObject.set(this.state.json)
-        }, 800)
+            this.jsonObject.set(getLocalJSONValue())
+        } ,0)
+       
     }
-    getLocalJSONValue = () => {
-        try {
-            let value = localStorage.getItem('json-local-value') || ''
-            return JSON.parse(value)
-        } catch (e) {
-            return {}
-        }
-    }
-    setLocalJSONValue = (value) => {
-        let raws = JSON.stringify(value)
-        localStorage.setItem('json-local-value', raws)
-    }
+   
     toGoStruct = () => {
         let result = jsonToGo(JSON.stringify(this.jsonObject.get()), null, null, false);
         this.setState({
@@ -73,7 +87,7 @@ class Convert extends React.Component {
                 const data = new Uint8Array(e.target.result);
                 const workbook = XLSX.read(data, { type: 'array', cellHTML: false });
                 let jsonData = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], { header: 1 });
-                this.jsonObject.set(this.intoObjectList(jsonData))
+                this.jsonObject.set(intoObjectList(jsonData))
             };
             reader.readAsArrayBuffer(file);
         }
@@ -107,19 +121,7 @@ class Convert extends React.Component {
             reader.readAsArrayBuffer(file);
         }
     }
-    intoObjectList = (list) => {
-        let retData = []
-        for (var i in list) {
-            if (i > 0) {
-                let tmp = {}
-                for (var j in list[0]) {
-                    tmp[list[0][j]] = list[i][j]
-                }
-                retData.push(tmp)
-            }
-        }
-        return retData
-    }
+    
     render() {
         return <div>
             <div style={{ textAlign: "center", marginBottom: "15px" }}>
@@ -129,9 +131,7 @@ class Convert extends React.Component {
                     <Button onClick={this.importFromFile} type="primary">文件导入</Button>
                 </Space>
             </div>
-            <JSONEditor height={'calc(100vh - 150px)'} ref={(e) => {
-                this.jsonObject = e
-            }} onValidate={(val) => this.setLocalJSONValue(val)} />
+            <JSONEditor height={'calc(100vh - 150px)'} ref={this.initJSONEditor} onValidate={(val) => setLocalJSONValue(val)} />
             <div style={{ textAlign: "center", marginTop: "15px" }}>
                 <Space>
                     <Button
@@ -179,7 +179,7 @@ class Convert extends React.Component {
                 }}
             >
                 <CopyToClipboard text={this.state.convert}>复制</CopyToClipboard>
-                <pre>{this.state.convert}</pre>
+                <pre style={{background:'rgb(245 244 244)', marginTop:'4px'}}>{this.state.convert}</pre>
             </Modal>
         </div>
     }
